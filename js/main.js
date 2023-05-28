0f0092592fae95edd28b73c51641d7975f3d0f6b
Firdos Akhundov <126233755+akhundov300@users.noreply.github.com>
2023-05-29 01:39:42 +0400

    Add files via upload
    Add animation and button back to top.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 // Custom Scroll
 document.querySelector('.custom-scroll').style.scrollBehavior = 'smooth';
+
+
+/*-- Back To Top---*/
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
